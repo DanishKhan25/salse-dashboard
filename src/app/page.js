7ac4084ca3a60614e105b7e44c41863aba5f7486
Danish <danishkhan25nov@gmail.com>
2024-04-04 01:28:24 +0530

    build error resolved

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 "use client";
 import BoxRow from "@/components/boxes/BoxRow";
-import DonutChart from "@/components/chart/DonutChart";
 import SalesChart from "@/components/chart/SalesChart";
 import Header from "@/components/header/Header";
 import Sidebar from "@/components/sidebar/Sidebar";
 import SalesTable from "@/components/tables/SalesTable";
+import dynamic from "next/dynamic";
 import styles from "./page.module.css";
+
+const DonutChart = dynamic(() => import("@/components/chart/DonutChart"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
@@ -20,9 +24,7 @@ export default function Home() {
           <div className={styles.chart1}>
             <SalesChart />
           </div>
-          <div className={styles.chart2}>
-            <DonutChart />
-          </div>
+          <div className={styles.chart2}>{/* <DonutChart /> */}</div>
         </div>
         <div className={styles.container}>
           <SalesTable />
